8ca670b0f30760f149f22b60f4cd3ca90c7d4d1d
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-12-30 03:38:09 -0800

    v4.0, made with Angular and Bootstrap

---
+++ modules/core.js
@@ -0,0 +1,81 @@
+﻿angular.module('core', [])
+    .factory('currentView', currentView)
+    .directive('ratActiveNav', ratActiveNav)
+    .directive('ratMenuId', ratMenuId);
+
+function currentView()
+{
+    var activeNavId = '';
+    var activeTitleText = '';
+
+    var retObj =
+        {
+            setCurrentView: function(navId, titleText)
+            {
+                activeNavId = navId;
+                activeTitleText = titleText;
+            },
+            getActiveNavId: function()
+            {
+                return activeNavId;
+            },
+            getTitleText: function()
+            {
+                return activeTitleText;
+            }
+        };
+
+    return retObj;
+}
+
+function ratActiveNav(currentView)
+{
+    return function(scope, element, attrs)
+    {
+        var activeNavId = attrs["ratActiveNav"];
+        var activeTitle = attrs["ratActiveTitle"];
+        currentView.setCurrentView(activeNavId, activeTitle);
+    };
+}
+
+function ratMenuId(currentView)
+{
+    var navElements = [];
+
+    function setActive(element, navId)
+    {
+        if (currentView.getActiveNavId() == navId)
+        {
+            element.addClass('active');
+        }
+        else
+        {
+            element.removeClass('active');
+        }
+    }
+
+    return function(scope, element, attrs)
+    {
+        var menuId = attrs["ratMenuId"];
+
+        navElements.push(
+            {
+                id: menuId,
+                node: element
+            });
+
+        var watcherFn = function(watchScope)
+        {
+            return watchScope.$eval('getActiveNavId()');
+        };
+        scope.$watch(watcherFn, function(newValue, oldValue)
+        {
+            for (var i = 0; i < navElements.length; i++)
+            {
+                var navElement = navElements[i];
+                setActive(navElement.node, navElement.id);
+            }
+        });
+    };
+}
+
---
+++ modules/pages.js
@@ -0,0 +1,27 @@
+﻿angular.module('pages', [])
+    .controller('aboutCtrl', AboutCtrl)
+    .controller('skillsCtrl', SkillsCtrl)
+    .controller('experienceCtrl', ExperienceCtrl)
+    .controller('projectsCtrl', ProjectsCtrl)
+    .controller('contactMeCtrl', ContactMeCtrl);
+
+function AboutCtrl(currentView)
+{
+}
+
+function SkillsCtrl(currentView)
+{
+}
+
+function ExperienceCtrl(currentView)
+{
+}
+
+function ProjectsCtrl(currentView)
+{
+}
+
+function ContactMeCtrl(currentView)
+{
+}
+
