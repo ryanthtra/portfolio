d5360fa243dc2d43bffe1ea39661db1e97f3a59d
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-12-30 17:58:36 -0800

    More responsive projects page

---
+++ models/projects.js
@@ -0,0 +1,47 @@
+﻿var projects = [
+    {
+        linkUrl: 'http://ryanthtra.github.io/mboutique',
+        thumbnail: 'images/mboutique.png',
+        label: 'MBoutique'
+    },
+    {
+        linkUrl: 'http://ryanthtra.github.io/memory_match',
+        thumbnail: 'images/matcheffect.png',
+        label: 'Memory Match'
+    },
+    {
+        linkUrl: 'http://ryanthtra.github.io/SGT',
+        thumbnail: 'images/sgt.png',
+        label: 'Student Grade Table'
+    },
+    {
+        linkUrl: 'http://ryanthtra.github.io/calculator',
+        thumbnail: 'images/calculator.png',
+        label: 'Calculator'
+    },
+    {
+        linkUrl: 'http://ryanthtra.github.io/hacka-tictactoe-athon',
+        thumbnail: 'images/matcheffect.png',
+        label: 'TTT Hackathon'
+    },
+    {
+        linkUrl: '#/projects',
+        thumbnail: '',
+        label: ''
+    },
+    {
+        linkUrl: '#/projects',
+        thumbnail: '',
+        label: ''
+    },
+    {
+        linkUrl: '#/projects',
+        thumbnail: '',
+        label: ''
+    },
+    {
+        linkUrl: '#/projects',
+        thumbnail: '',
+        label: ''
+    }
+];
--- modules/pages.js
+++ modules/pages.js
@@ -17,8 +17,37 @@ function ExperienceCtrl(currentView)
 {
 }
 
-function ProjectsCtrl(currentView)
+function ProjectsCtrl($scope, currentView)
 {
+    $scope.projects = projects;
+    $scope.getProjectLink = getProjectLink;
+    $scope.getProjectThumbnail = getProjectThumbnail;
+    $scope.getProjectLabel = getProjectLabel;
+
+    $scope.blinkFrame = function(index)
+    {
+        var frame = $('#project-gallery').find('.project-frame-blink')[index];
+
+        $(frame).addClass("show-blink");
+    }
+    $scope.unblinkFrame = function(index)
+    {
+        var frame = $('#project-gallery').find('.project-frame-blink')[index];
+
+        $(frame).removeClass("show-blink");
+    }
+    function getProjectLink(index)
+    {
+        return projects[index].linkUrl;
+    }
+    function getProjectThumbnail(index)
+    {
+        return projects[index].thumbnail;
+    }
+    function getProjectLabel(index)
+    {
+        return projects[index].label;
+    }
 }
 
 function ContactMeCtrl(currentView)
