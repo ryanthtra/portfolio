06ab76c966f4e4c414d5eb3d8e77bd25b10bca34
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2016-04-05 15:24:03 -0700

    Small change for preloading images

--- billboard.js
+++ billboard.js
@@ -32,12 +32,13 @@ Billboard.prototype.flip = function($clickedMenuItem)
 		
     $('.billboard').toggleClass('flip-horiz');
     
-    setTimeout(function()
-    {
-        $('.' + otherside).addClass('hidden');
-    }, 280);
+    // setTimeout(function()
+    // {
+    //     $('.' + otherside).addClass('hidden');
+    // }, 280);
 	setTimeout(function()
     {
+		$('.' + otherside).addClass('hidden');
         $newBack.removeClass('hidden');
     }, 230);
 	// $('.' + otherside).addClass('hidden');
